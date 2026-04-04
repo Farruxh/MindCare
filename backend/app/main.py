d45e7d47acb1093a2e4ffa802bb9820c83a43291
Farruxh <farrukh.web2@gmail.com>
2026-04-05 00:40:25 +0500

    feat: update main with new router, and model import

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,7 @@ from app.models.user import User
 from app.models.assessment import Assessment_Result
 from app.models.chat_history import Chat_History
 from app.models.message import Message
+from app.models.clinic import Clinic
 from app.settings import settings
 
 
@@ -27,11 +28,13 @@ from app.routes import user
 from app.routes import assessment
 from app.routes import chat_history
 from app.routes import message
+from app.routes import clinic
 
 app.include_router(user.router)
 app.include_router(assessment.router)
 app.include_router(chat_history.router)
 app.include_router(message.router)
+app.include_router(clinic.router)
 
 
 @app.exception_handler(HTTPException)
