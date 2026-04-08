ed5c6765206ad44271c9b7198c575149be7eb8d3
Farruxh <farrukh.web2@gmail.com>
2026-04-08 17:25:57 +0500

    feat: add clinic module and alembic migration

---
+++ backend/app/models/clinic.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime, func
+from app.db import Base
+
+class Clinic(Base):
+    __tablename__ = "Clinic"
+    clinic_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String, index=True)
+    location = Column(String)
+    contact_no = Column(String)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
