ec7292eb60276337dfb1a36129ff593a7d1cabb5
Farruxh <farrukh.web2@gmail.com>
2026-04-20 17:09:29 +0500

    feat: implement global state and core feature routes

--- frontend/src/app/AppRoutes.tsx
+++ frontend/src/app/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import { SignUpPage } from "./components/pages/SignUpPage.tsx";
 import { LoginPage } from "./components/pages/LoginPage.tsx";
@@ -6,14 +6,13 @@ import { LandingPage } from "./components/pages/LandingPage.tsx";
 import { Dashboard } from "./components/pages/Dashboard.tsx";
 import { ProfilePage } from "./components/pages/ProfilePage.tsx";
 import { ForgotPassword } from "./components/pages/ForgotPassword.tsx";
-import VerifyPasswordToken from "./components/pages/VerifyPasswordToken.tsx";
+import { VerifyPasswordToken } from "./components/pages/VerifyPasswordToken.tsx";
 import { ResetPassword } from "./components/pages/ResetPassword.tsx"
 import { ChatInterface } from "./components/pages/ChatInterface.tsx";
 import { SelfAssessment } from "./components/pages/SelfAssessment.tsx";
 import { DailyJournal } from "./components/pages/DailyJournal.tsx"
 import { MeditationPage } from "./components/pages/MeditationPage.tsx";
 import { ClinicLocator } from "./components/pages/ClinicLocator.tsx"
-import { ThemeProvider } from "./context/ThemeContext.tsx";
 import { ProtectedRoute } from "./components/ProtectedRoute.tsx";
 
 export default function AppRoutes() {
@@ -29,25 +28,15 @@ export default function AppRoutes() {
                     { path: "forgot-password", element: <ForgotPassword /> },
                     { path: "verify-token", element: <VerifyPasswordToken /> },
                     { path: "reset-password", element: <ResetPassword /> },
-
-                    {
-                        element:
-                            (<ThemeProvider>
-                                <Outlet />
-                            </ThemeProvider>),
-                        children: [
-                            { path: "dashboard", element: <ProtectedRoute> <Dashboard /> </ProtectedRoute>},
-                            { path: "assistant/:chat_id", element:<ProtectedRoute> <ChatInterface /> </ProtectedRoute> },
-                            { path: "assessment", element:<ProtectedRoute> <SelfAssessment /> </ProtectedRoute> },
-                            { path: "daily-journal", element: <ProtectedRoute> <DailyJournal /> </ProtectedRoute> },
-                            { path: "meditation", element: <ProtectedRoute> <MeditationPage /> </ProtectedRoute> },
-                            { path: "clinics", element: <ProtectedRoute> <ClinicLocator /> </ProtectedRoute> },
-                            { path: "profile", element: <ProtectedRoute> <ProfilePage /> </ProtectedRoute> }
-                        ]
-                    }
+                    { path: "dashboard", element: <ProtectedRoute> <Dashboard /> </ProtectedRoute> },
+                    { path: "assistant/:chat_id", element: <ProtectedRoute> <ChatInterface /> </ProtectedRoute> },
+                    { path: "assessment", element: <ProtectedRoute> <SelfAssessment /> </ProtectedRoute> },
+                    { path: "daily-journal", element: <ProtectedRoute> <DailyJournal /> </ProtectedRoute> },
+                    { path: "meditation", element: <ProtectedRoute> <MeditationPage /> </ProtectedRoute> },
+                    { path: "clinics", element: <ProtectedRoute> <ClinicLocator /> </ProtectedRoute> },
+                    { path: "profile", element: <ProtectedRoute> <ProfilePage /> </ProtectedRoute> }
                 ]
             }
-        ])}
-        />
+        ])} />
     )
 }
