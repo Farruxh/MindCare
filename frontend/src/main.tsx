82fea67bbbee680ed32c4cd1b1165cea271f1a7b
Farruxh <farrukh.web2@gmail.com>
2026-05-03 12:35:53 +0500

    feat: implement dark mode with db persistence and theme provider in protected routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,12 @@ import { AuthProvider } from "./app/context/AuthContext.tsx"
 import AppRoutes from "./app/AppRoutes.tsx"
 import "./styles/index.css";
 import GlobalAlert from "./app/components/Global/GlobalAlert.tsx";
-import { ThemeProvider } from "./app/context/ThemeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-    <ThemeProvider>
-        <AlertProvider>
-            <AuthProvider>
+    <AlertProvider>
+        <AuthProvider>
                 <AppRoutes />
                 <GlobalAlert />
-            </AuthProvider>
-        </AlertProvider>
-    </ThemeProvider>
+        </AuthProvider>
+    </AlertProvider>
 );
